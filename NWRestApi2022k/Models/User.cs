7c00c110e749ef1adb1756ab104ab3b98500312c
Sami Palomäki <96203498+Sami19841Careeria@users.noreply.github.com>
2022-12-10 13:17:42 +0200

    takaisin muutokset...

--- NWRestApi2022k/Models/User.cs
+++ NWRestApi2022k/Models/User.cs
@@ -6,11 +6,11 @@ namespace NWRestApi2022k.Models
     public partial class User
     {
         public int UserId { get; set; }
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string? Email { get; set; }
-        public string? UserName { get; set; }
-        public string? Password { get; set; } = "aijsoiwoinswoiswois";
-        public int? AccesslevelId { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; } = "aijsoiwoinswoiswois";
+        public int AccesslevelId { get; set; }
     }
 }
